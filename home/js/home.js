3dd7bd82662afc4a680811f9c9ce91b6561c47e8
Nutthidapops_ <73658351+63070052@users.noreply.github.com>
2021-11-11 23:12:53 +0700

    Add files via upload

---
+++ home/js/home.js
@@ -0,0 +1,139 @@
+
+    function mouseOver() {
+      document.getElementById("bg-donut").style.opacity = "1";
+      document.getElementById("bg-donut").style.transition = "0.3s";
+    }
+    
+    function mouseOut() {
+      document.getElementById("bg-donut").style.opacity = "0";
+    }
+// ------------------------------------------------------------------
+    function mouseOver1() {
+      document.getElementById("bg-donut1").style.opacity = "1";
+      document.getElementById("bg-donut1").style.transition = "0.3s";
+    }
+    
+    function mouseOut1() {
+      document.getElementById("bg-donut1").style.opacity = "0";
+    }
+// ------------------------------------------------------------------
+    function mouseOver2() {
+        document.getElementById("bg-donut2").style.opacity = "1";
+        document.getElementById("bg-donut2").style.transition = "0.3s";
+    }
+      
+    function mouseOut2() {
+        document.getElementById("bg-donut2").style.opacity = "0";
+    }
+// ------------------------------------------------------------------
+      function mouseOver3() {
+        document.getElementById("bg-donut3").style.opacity = "1";
+        document.getElementById("bg-donut3").style.transition = "0.3s";
+      }
+      
+      function mouseOut3() {
+        document.getElementById("bg-donut3").style.opacity = "0";
+      }
+// ------------------------------------------------------------------------------------
+// ----------------popup--------------------------------------------------------------------
+
+function onclickmenu(){
+  document.getElementById("main-popup").style.opacity = "10";
+  document.getElementById("main-popup").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu(){
+  document.getElementById("main-popup").style.opacity = "-1";
+  document.getElementById("main-popup").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu1(){
+  document.getElementById("main-popup1").style.opacity = "10";
+  document.getElementById("main-popup1").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu1(){
+  document.getElementById("main-popup1").style.opacity = "-1";
+  document.getElementById("main-popup1").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu2(){
+  document.getElementById("main-popup2").style.opacity = "10";
+  document.getElementById("main-popup2").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu2(){
+  document.getElementById("main-popup2").style.opacity = "-1";
+  document.getElementById("main-popup2").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu3(){
+  document.getElementById("main-popup3").style.opacity = "10";
+  document.getElementById("main-popup3").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu3(){
+  document.getElementById("main-popup3").style.opacity = "-1";
+  document.getElementById("main-popup3").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+function onclickmenu_1(){
+  document.getElementById("main-popup-1").style.opacity = "10";
+  document.getElementById("main-popup-1").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu_1(){
+  document.getElementById("main-popup-1").style.opacity = "-1";
+  document.getElementById("main-popup-1").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu_2(){
+  document.getElementById("main-popup-2").style.opacity = "10";
+  document.getElementById("main-popup-2").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu_2(){
+  document.getElementById("main-popup-2").style.opacity = "-1";
+  document.getElementById("main-popup-2").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu_3(){
+  document.getElementById("main-popup-3").style.opacity = "10";
+  document.getElementById("main-popup-3").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu_3(){
+  document.getElementById("main-popup-3").style.opacity = "-1";
+  document.getElementById("main-popup-3").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+function onclickmenu_4(){
+  document.getElementById("main-popup-4").style.opacity = "10";
+  document.getElementById("main-popup-4").style.zIndex = "20";
+  document.getElementById("bodys").style.overflowY = "hidden";
+}
+
+function outclickmenu_4(){
+  document.getElementById("main-popup-4").style.opacity = "-1";
+  document.getElementById("main-popup-4").style.zIndex = "-20";
+  document.getElementById("bodys").style.overflowY = "visible";
+}
+
+
